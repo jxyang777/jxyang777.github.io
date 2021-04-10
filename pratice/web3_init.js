a66824cc984c8daf7ed64c8307e954557219eab4
jxyang <44730359+jxyang777@users.noreply.github.com>
2021-04-10 16:26:30 +0800

    Update web3_init.js

--- pratice/web3_init.js
+++ pratice/web3_init.js
@@ -19,3 +19,20 @@ async function send() {
         console.error("Error:", err);
     }
 };
+
+async function start() {
+    try {
+        let defaultAccount = await web3.eth.getCoinbase();
+        // let defaultAccount = "0xB31BFC1a6DE8a0228070263CD697E989c1838d71";
+        let balance = await web3.eth.getBalance(defaultAccount);
+
+        var html_account = document.getElementById("account");
+        var html_balance = document.getElementById("balance");
+        html_account.textContent = defaultAccount;
+        html_balance.textContent = web3.utils.fromWei(balance, "ether");
+    } catch(err) {
+        console.error("Error:", err);
+    }
+}
+
+window.addEventListener("load", start);
