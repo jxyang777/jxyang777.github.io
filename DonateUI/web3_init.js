663b6cd2d42e0c2c9b673b0dd8eb1d596d0aec1f
jxyang <44730359+jxyang777@users.noreply.github.com>
2021-04-10 15:12:24 +0800

    Update web3_init.js

--- DonateUI/web3_init.js
+++ DonateUI/web3_init.js
@@ -1,31 +1,21 @@
 if (window.ethereum) {
-        window.web3 = new Web3(window.ethereum);
-        window.ethereum.enable();
+    web3 = new Web3(window.ethereum);
+    window.ethereum.enable();
 }
-/* else {
-    // set the provider you want from Web3.providers
+else {
     web3 = new Web3(new Web3.providers.HttpProvider("http://192.168.50.75:8545"));
-} */
-
-// if (typeof provider !== 'undefined') {
-//     provider = new ethers.providers.Web3Provider(window.ethereum)
-// }
-// else {
-//     provider = new ethers.providers.JsonRpcProvider("http://192.168.50.75:8545");
-// }
-
-// const provider = new ethers.providers.Web3Provider(window.ethereum)
+}
 
 async function start() {
     try {
-        let defaultAccount = await window.web3.eth.getCoinbase();
+        let defaultAccount = await web3.eth.getCoinbase();
         // let defaultAccount = "0xB31BFC1a6DE8a0228070263CD697E989c1838d71";
-        let balance = await window.web3.eth.getBalance(defaultAccount);
+        let balance = await web3.eth.getBalance(defaultAccount);
 
         var html_account = document.getElementById("account");
         var html_balance = document.getElementById("balance");
         html_account.textContent = defaultAccount;
-        html_balance.textContent = window.web3.utils.fromWei(balance, "ether");
+        html_balance.textContent = web3.utils.fromWei(balance, "ether");
     } catch(err) {
         console.error("Error:", err);
     }
