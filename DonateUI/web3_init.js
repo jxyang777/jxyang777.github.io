a1b1c4bff8362eae384dd8b4263d70c826431ac9
jxyang <44730359+jxyang777@users.noreply.github.com>
2021-04-10 14:14:37 +0800

    Update web3_init.js

--- DonateUI/web3_init.js
+++ DonateUI/web3_init.js
@@ -1,14 +1,24 @@
 if (typeof web3 !== 'undefined') {
-        web3 = new Web3(web3.currentProvider);
+        web3 = new Web3(windows.ethereum);
 }
 else {
     // set the provider you want from Web3.providers
     web3 = new Web3(new Web3.providers.HttpProvider("http://192.168.50.75:8545"));
 }
+
+// if (typeof provider !== 'undefined') {
+//     provider = new ethers.providers.Web3Provider(window.ethereum)
+// }
+// else {
+//     provider = new ethers.providers.JsonRpcProvider("http://192.168.50.75:8545");
+// }
+
+// const provider = new ethers.providers.Web3Provider(window.ethereum)
+
 async function start() {
     try {
-        let defaultAccount = await web3.eth.getCoinbase();
-        // let defaultAccount = "0xB31BFC1a6DE8a0228070263CD697E989c1838d71";
+        // let defaultAccount = await web3.eth.getCoinbase();
+        let defaultAccount = "0xB31BFC1a6DE8a0228070263CD697E989c1838d71";
         let balance = await web3.eth.getBalance(defaultAccount);
 
         var html_account = document.getElementById("account");
