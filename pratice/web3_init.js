6eca33bf36a2b352647bce38d5ec269680d8e2fe
jxyang <44730359+jxyang777@users.noreply.github.com>
2021-04-10 16:07:25 +0800

    Update web3_init.js

--- pratice/web3_init.js
+++ pratice/web3_init.js
@@ -6,11 +6,12 @@ else {
     web3 = new Web3(new Web3.providers.HttpProvider("http://192.168.50.75:8545"));
 }
 
-let toAccount = $("#address").val();
-let value = $("#money").val();
+
 
 async function send() {
     let defaultAccount = await web3.eth.getCoinbase();
+    let toAccount = $("#address").val();
+    let value = $("#money").val();
     var tranHash = web3.eth.sendTransaction({ from: defaultAccount , to:toAccount, gas: value});
     document.querySelector("#TransactionHash").innerHTML = "Transaction Hash: " + tranHash;
 };
