6647747eb83d657859edd7e90aafad3f6e87df75
jxyang <44730359+jxyang777@users.noreply.github.com>
2021-04-10 15:41:47 +0800

    Update web3_init.js

--- pratice/web3_init.js
+++ pratice/web3_init.js
@@ -1,27 +1,10 @@
-if (typeof web3 !== 'undefined') {
-        web3 = new Web3(web3.currentProvider);
+if (window.ethereum) {
+    web3 = new Web3(window.ethereum);
+    window.ethereum.enable();
 }
 else {
-    // set the provider you want from Web3.providers
     web3 = new Web3(new Web3.providers.HttpProvider("http://192.168.50.75:8545"));
 }
-/* async function start() {
-    try {
-        // let defaultAccount = await web3.eth.getCoinbase();
-        let defaultAccount = "0xB31BFC1a6DE8a0228070263CD697E989c1838d71";
-        let balance = await web3.eth.getBalance(defaultAccount);
-
-        var html_account = document.getElementById("account");
-        var html_balance = document.getElementById("balance");
-        html_account.textContent = defaultAccount;
-        html_balance.textContent = web3.utils.fromWei(balance, "ether");
-    } catch(err) {
-        console.error("Error:", err);
-    }
-    }
-
-window.addEventListener("load", start); */
-
 
 async function send() {
     let defaultAccount = await web3.eth.getCoinbase();
