6902ec605ba9d96ec0b9983d4a952585060862a8
jxyang <44730359+jxyang777@users.noreply.github.com>
2021-04-10 16:03:50 +0800

    Update web3_init.js

--- pratice/web3_init.js
+++ pratice/web3_init.js
@@ -5,11 +5,12 @@ if (window.ethereum) {
 else {
     web3 = new Web3(new Web3.providers.HttpProvider("http://192.168.50.75:8545"));
 }
-let defaultAccount = await web3.eth.getCoinbase();
-    let account = $("#address").val();
-    let gas = $("#money").val();
+
+let account = $("#address").val();
+let gas = $("#money").val();
 
 async function send() {
+    let defaultAccount = await web3.eth.getCoinbase();
     var tranHash = web3.eth.sendTransaction({ from: defaultAccount , to:account, gas: gas});
     document.querySelector("#TransactionHash").innerHTML = "Transaction Hash: " + tranHash;
 };
