e8efa6cd8cd9535439518c0e5a87842f7d848d7f
jxyang <44730359+jxyang777@users.noreply.github.com>
2021-04-11 14:31:07 +0800

    Update web3_init.js

--- pratice/web3_init.js
+++ pratice/web3_init.js
@@ -7,7 +7,7 @@ else {
 }
 
 $("#send").click(function () {
-    let defaultAccount = web3.eth.getCoinbase();
+    //let defaultAccount = web3.eth.getCoinbase();
     // var result=web3.eth.sendTransaction({ from:defaultAccount , to:"0x36bfedb791f66427327330ad4a1c7a11aadedce2", value:50 });
 });
 
