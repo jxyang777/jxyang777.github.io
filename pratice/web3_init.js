817f565d03a238ee6a951c3b7e40c101e8630e29
jxyang <44730359+jxyang777@users.noreply.github.com>
2021-04-11 17:06:31 +0800

    Update web3_init.js

--- pratice/web3_init.js
+++ pratice/web3_init.js
@@ -23,15 +23,12 @@ async function start() {
 }
 window.addEventListener("load", start);
 
-let accepterAddr = "geieoh";
+let accepterAddr = "";
 let amount = 20;
 $("#send").click(function () {
-    accepterAddr = "0x36bfedb791f66427327330ad4a1c7a11aadedce2";
+    // accepterAddr = "0x36bfedb791f66427327330ad4a1c7a11aadedce2";
     accepterAddr = $("#account").val();
-
-    var html_acc = document.getElementById("to");
-    html_acc.textContent = accepterAddr;
-    
     amount = web3.utils.toWei($("#money").val(), "ether");
+    
     // var hash = web3.eth.sendTransaction({ from:defaultAccount , to:accepterAddr, value:amount});
 });
